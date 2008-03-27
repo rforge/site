b9107284159428ee908c33591ec825eb761454b1
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-03-27 19:31:33 +0000

    fixed package building bug caused by malformed DESCRIPTION files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/site@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/update_package_db.R
+++ pkg/R/update_package_db.R
@@ -6,37 +6,54 @@
 update_package_library <- function(pkgs, path_to_pkg_src, repository_url, lib, ...){
   ## first update all installed packages if necessary
   update.packages(lib = lib, repos = repository_url, ask = FALSE)
-  
-  ##source("${R_scripts_dir}/packages.R")
-  ##source("${R_scripts_dir}/R_Forge_utils.R")
-  ##dir <- file_path_as_absolute(getwd())
 
+  ## pkg list and dependency structure
   pkgs_dep <- resolve_dependency_structure(pkgs, repository_url, path_to_pkg_src)
+
   ## install missing packages
   pkgs_installed <- installed.packages(lib = lib)
   ## Temporarily All packages are installed
-  ## TODO: install only those packages which are only available from R-Forge, the rest should be installed from R-Forge considering the install order
+  ## TODO: install only those packages which are only available from R-Forge, the rest
+  ## should be installed from CRAN or other repositories considering the install order
   pkgs_to_install <- setdiff(pkgs_dep[["ALL"]], pkgs_installed)
   if(length(pkgs_to_install) >= 2)
     install.packages(pkgs_to_install, lib = lib, contriburl = contrib.url(repository_url), ...)
 }
 
-
+## this function resolves the dependency structure of source pkgs and returns a list
+## containing ALL packages, packages from CRAN and the install order of ALL packages
+## Additionally suggested packages are included, as they are probably needed when
+## building package vignettes
 resolve_dependency_structure <- function(pkgs, repository_url, path_to_pkg_src){
-  ## before creating the source repository we have to check the DESCRIPTION files for validity
-  check_description_and_remove_packages_not_passed(pkgs, path_to_pkg_src)
-  ## create PACKAGES from R-Forge source dirs
-  write_PACKAGES(dir = path_to_pkg_src, type = "source",
-                 fields = tools:::.get_standard_repository_db_fields(),
-                 unpacked = TRUE) 
+  ## different file separators according to platform
+  path_separator <- c(unix = "/", windows = "\\")
+  path_separator <- path_separator[.Platform$OS.type]
+
+  ## build pkg database out of DESCRIPTION files
+  pkg_db <- tools:::.build_repository_package_db(path_to_pkg_src, unpacked = TRUE)
+  ## packages probably not working
+  pkgs_defunct <- NULL
+  ## which packages have bad DESCRIPTION files?
+  for(pkg in pkgs){
+    pkg_DESCRIPTION <- paste(path_to_pkg_src, pkg, "DESCRIPTION", sep = path_separator)
+    if(file.exists(pkg_DESCRIPTION)){
+      checked <- tools:::.check_package_description(pkg_DESCRIPTION)
+      if(length(checked) > 0)
+        pkgs_defunct <- c(pkgs_defunct, pkg)
+    }
+  }
+  ## remove defunct packages from package db
+  if(!is.null(pkgs_defunct))
+    pkg_db <- pkg_db[!is.element(names(pkg_db), pkgs_defunct)]
+   
+  ## create PACKAGES in source dir
+  write.dcf(do.call(rbind, pkg_db), file = paste(path_to_pkg_src, "PACKAGES", sep = path_separator))
+ 
   ## look out for available packages
   avail_cran <- available.packages(contriburl =
                                    contrib.url(repository_url))
-  avail_try<- try(avail_rforge <- available.packages(contriburl =
-                                     sprintf("file:///%s", path_to_pkg_src)))
-  if(inherits(avail_try, "try-error"))
-    stop("Package DB cannot be properly updated as there are malformed entries
-          in one of the DESCRIPTION files!")
+  avail_rforge <- available.packages(contriburl =
+                                     sprintf("file:///%s", path_to_pkg_src))
   avail <- rbind(avail_rforge, avail_cran)
   ## What packages do we need from external repository
   pkgs <- pkgs[pkgs %in% rownames(avail_rforge)]
@@ -48,36 +65,7 @@ resolve_dependency_structure <- function(pkgs, repository_url, path_to_pkg_src){
   
   DL <- utils:::.make_dependency_list(pkgs_all, avail)
   pkgs_install_order <- utils:::.find_install_order(pkgs_all, DL)
+
   ## return a vector with packages and install order
   list(ALL = pkgs_all, CRAN = pkgs_cran, INSTALL_ORDER = pkgs_install_order)
-}
-
-check_description_and_remove_packages_not_passed <- function(pkgs, path_to_pkg_src){
-  path_separator <- c(unix = "/", windows = "\\")
-  path_separator <- path_separator[.Platform$OS.type]
-  currentR <- getRversion()
-  for(pkg in pkgs){
-    pkg_DESCRIPTION <- paste(path_to_pkg_src, pkg, "DESCRIPTION", sep = path_separator)
-    if(file.exists(pkg_DESCRIPTION)){
-      dcf <- read.dcf(pkg_DESCRIPTION)
-      R_version_try <- try(.checkRversion(dcf[1,], currentR))
-      if(inherits(R_version_try, "try-error")){
-        warning(paste("Package", pkg, "has malformed version number in DESCRIPTION file -> removing!"))
-        system(paste("rm -rf", paste(path_to_pkg_src, pkg, sep = path_separator)))
-      }
-    }
-  }
-}
-
-## extracted from available.packages for checking desription files as we don't know if
-## some packages have valid DESCRIPTION files or not apriori. This possibly can break
-## the build process if there are invalid version numbers.
-
-.checkRversion <- function(x, currentR) {
-  if (is.na(xx <- x["Depends"]))
-    return(TRUE)
-  xx <- tools:::.split_dependencies(xx)
-  if (length(z <- xx[["R", exact = TRUE]]) > 1)
-    eval(parse(text = paste("currentR", z$op, "z$version")))
-  else TRUE
 }
