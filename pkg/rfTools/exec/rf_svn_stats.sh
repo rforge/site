8d806a6b1238afb01d9ca48267bc285ae8171118
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-14 23:04:10 +0000

    fixed encoding and now exports as .xml
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/site@309 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rfTools/exec/rf_svn_stats.sh
+++ pkg/rfTools/exec/rf_svn_stats.sh
@@ -4,6 +4,6 @@ dirs=`find /srv/svn/ -maxdepth 1 -type d | sort`
 for d in $dirs; do \
   p=`basename $d`
   echo -n "Getting svn logs for project $p ..."
-  svn log $@ file://$d > ~/tmp/svnlogs/`basename $d`.log
+  svn log $@ file://$d > /srv/rf/logs/analysis/svnlogs/`basename $d`.xml
   echo " done";
 done
